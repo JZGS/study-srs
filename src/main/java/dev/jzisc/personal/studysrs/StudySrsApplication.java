f02a35462f76a652cb68d20a2094ecf1d8f29a70
JZGS <jcarlosgs1316@gmail.com>
2021-08-16 12:01:41 -0500

    bootstrap project

---
+++ src/main/java/dev/jzisc/personal/studysrs/StudySrsApplication.java
@@ -0,0 +1,13 @@
+package dev.jzisc.personal.studysrs;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StudySrsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StudySrsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/dev/jzisc/personal/studysrs/StudySrsApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.jzisc.personal.studysrs;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StudySrsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
